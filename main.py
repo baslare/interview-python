e989c08b19d6962358465425e7b7b63a840137bd
efe <efe.baslar@gmail.com>
2023-05-30 13:29:43 +0200

    fixed bugs, added a feature

--- main.py
+++ main.py
@@ -33,19 +33,20 @@ def prepare_forecast():
     ts = client.query_wind_and_solar_forecast(country_code, start=start, end=end)
     ts.to_csv('data/forecast.csv')
 
-def get_weather_forecast():
-    r = requests.get(f"https://archive-api.open-meteo.com/v1/era5?latitude={cities['Madrid']['latitude']}&longitude={cities['Madrid']['longitude']}&start_date=2022-10-09&end_date=2022-10-11&hourly=temperature_2m,windspeed_10m")
+def get_weather_forecast(city):
+    r = requests.get(f"https://archive-api.open-meteo.com/v1/era5?latitude={cities[city]['lat']}&longitude={cities[city]['long']}&start_date=2022-10-09&end_date=2022-10-11&hourly=temperature_2m,windspeed_100m")
+    print(f"https://archive-api.open-meteo.com/v1/era5?latitude={cities['Madrid']['lat']}&longitude={cities['Madrid']['long']}&start_date=2022-10-09&end_date=2022-10-11&hourly=temperature_2m,windspeed_100m")
     data = r.json()
     return data
 
-def enhance_generation_forecast(forecast):
+def enhance_generation_forecast(forecast,name):
     ts = pd.read_csv('data/forecast.csv', index_col=0)
     ts['temperature'] = None
     ts['windspeed'] = None
 
     for i, hour in enumerate(forecast['hourly']['time']):
         # TODO there seems to be a subtle bug here regarding https://delorean.readthedocs.io/en/latest/quickstart.html#ambiguous-cases
-        date = parse(hour, timezone=forecast['timezone_abbreviation'])
+        date = parse(hour, timezone=forecast['timezone_abbreviation'],dayfirst=False)
         date = date.shift("Europe/Madrid")
         key = date.datetime.strftime("%Y-%m-%d %H:%M:%S%z")
         key = key[:-2] + ':' + key[-2:]
@@ -53,12 +54,16 @@ def enhance_generation_forecast(forecast):
             ts.loc[[key], ['temperature']] = forecast['hourly']['temperature_2m'][i]
 
             #  TODO for good forecast of wind generation we should take the wind speed at 100m instead, turbines are way taller than 10m.
-            ts.loc[[key], ['windspeed']] = forecast['hourly']['windspeed_10m'][i]
+            ts.loc[[key], ['windspeed']] = forecast['hourly']['windspeed_100m'][i]
 
     ts = ts.fillna(method='pad')    
-    ts.to_csv('data/result.csv')
+    ts.to_csv(f"data/result{name}.csv")
 
 
 if __name__ == '__main__':
-    forecast = get_weather_forecast()
-    enhance_generation_forecast(forecast)
+
+
+    for key in cities.keys():
+        forecast = get_weather_forecast(key)
+        enhance_generation_forecast(forecast,key)
+
